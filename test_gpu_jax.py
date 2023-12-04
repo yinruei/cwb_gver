758506ab263b102565c163790c803fdcbd6611c2
yinruei <littleblacker29@gmail.com>
2023-12-04 03:47:12 +0000

    update jax code

--- test_gpu_jax.py
+++ test_gpu_jax.py
@@ -1,11 +1,14 @@
-import pandas as pd
-import numpy as np
 import os
+import numpy as np
 import time
 import nvtx
+import jax
+import jax.numpy as jnp
 
-# 記錄起始時間
-start_time = time.time()
+
+IS_CPU = int(os.environ.get("IS_CPU", 0))
+if IS_CPU >= 1:
+    jax.config.update('jax_platform_name', 'cpu')
 
 
 # Load the NumPy arrays
@@ -15,26 +18,23 @@ fcst = np.load("2023090500.npy")
 print('anl-->', anl.shape)
 print('fcst-->', fcst.shape)
 
-import jax
-import jax.numpy as jnp
-
 @jax.jit
-def compute_metrics(aa_arr, ff_arr):
+def compute_metrics(ff_arr, aa_arr):
     # MSE
-    mse_score = jnp.mean((aa_arr - ff_arr)**2)
+    mse_score = jnp.mean((ff_arr - aa_arr)**2)
     # RMSE
     rmse_score = jnp.sqrt(mse_score)
     # MAE
-    mae_score = jnp.mean(jnp.abs(aa_arr - ff_arr))
+    mae_score = jnp.mean(jnp.abs(ff_arr - aa_arr))
     # Bias
-    bias_score = jnp.mean(aa_arr - ff_arr)
+    bias_score = jnp.mean(ff_arr - aa_arr)
     return mse_score, rmse_score, mae_score, bias_score
 
 @jax.jit
-def vectorize_compute_metrics(aa_arr, ff_arr):
-    vmapped = jax.vmap(compute_metrics, in_axes=(None, 1))
+def vectorize_compute_metrics(ff_arr, aa_arr):
+    vmapped = jax.vmap(compute_metrics, in_axes=(1, None))
     mse_score, rmse_score, mae_score, bias_score = \
-        vmapped(aa_arr, ff_arr)
+        vmapped(ff_arr, aa_arr)
     return mse_score, rmse_score, mae_score, bias_score
 
 anl_jnp_arr = jnp.asarray(anl)
@@ -45,40 +45,20 @@ mae = []
 bias = []
 one_var_start_time = time.time()
 for i in range(10):
+    if i == 1:
+        one_var_start_after_compiling_time = time.time()
     nvtx.push_range("Compute score", domain="vmap_compute_score")
     _, rmse_score, mae_score, bias_score = \
-        vectorize_compute_metrics(anl_jnp_arr[:, i, :, :], fcst_tensor[:, i, :, :, :])
-    rmse.append(rmse_score)
-    mae.append(mae_score)
-    bias.append(bias_score)
-
+        vectorize_compute_metrics(fcst_tensor[:, i, :, :, :], anl_jnp_arr[:, i, :, :])
+    rmse.append(rmse_score.block_until_ready())
+    mae.append(mae_score.block_until_ready())
+    bias.append(bias_score.block_until_ready())
     nvtx.pop_range(domain="vmap_compute_score")
 one_var_end_time = time.time()
-print(f"One Variable Compute Teime: {one_var_end_time - one_var_start_time} sec")
-
-# nvtx.push_range("Write to CSV", domain="write_to_csv")
-# # 創建一個字典來保存指標數值
-# metrics_values = {
-#     'RMSE': rmse,
-#     'MAE': mae,
-#     'Bias': bias,
-# }
-
-# # 將字典轉換為 DataFrame
-# df_metrics = pd.DataFrame(metrics_values)
-
-# # 定義目標資料夾
-# output_folder = "score"
-
-# # 如果目標資料夾不存在，則創建它
-# if not os.path.exists(output_folder):
-#     os.makedirs(output_folder)
-
-# # 將 DataFrame 寫入 txt 檔案
-# txt_filename = os.path.join(output_folder, "metrics_results.txt")
-# df_metrics.to_csv(txt_filename, sep=' ', index=False)
-
-# print(f'Metrics results saved to {txt_filename}')
+one_var_time = one_var_end_time - one_var_start_time
+one_var_after_compiling_time = one_var_end_time - one_var_start_after_compiling_time
+print(f"One Variable Compute Time: {one_var_time} sec")
+print(f"One Variable Compute (Ignore Compiling) Time: {one_var_after_compiling_time} sec")
 
 @jax.jit
 def compute_predict_loss(pred, target, pred_obs, target_obs):
@@ -90,6 +70,12 @@ def compute_predict_loss(pred, target, pred_obs, target_obs):
     return brier_score, rpss_score, crpss_score
 
 # 假設你已經有實際觀測值和預測的機率，這裡使用隨機數生成
+brier = []
+rpss = []
+crpss = []
+
+total_pred_loss_time = 0
+total_pred_loss_after_compiling_time = 0
 for i in range(5): # Assume 10 set of data
     key = jax.random.PRNGKey(0)
     subkeys = jax.random.split(key, 2)
@@ -102,18 +88,25 @@ for i in range(5): # Assume 10 set of data
     y_obs_preds = jax.random.normal(subkeys[1], (30, 10, 71, 73, 144), jnp.float32)
 
     pred_loss_start_time = time.time()
+    nvtx.push_range("Compute Pred Loss", domain="compute_pred_loss")
     brier_score, rpss_score, crpss_score = compute_predict_loss(y_preds, y, y_obs_preds, y_obs)
+
+    brier.append(brier_score.block_until_ready())
+    rpss.append(rpss_score.block_until_ready())
+    crpss.append(crpss_score.block_until_ready())
+
+    nvtx.pop_range(domain="compute_pred_loss")
     pred_loss_end_time = time.time()
+    one_pred_loss_time = pred_loss_end_time - pred_loss_start_time
+    total_pred_loss_time += one_pred_loss_time
+    if i > 0:
+        total_pred_loss_after_compiling_time += one_pred_loss_time
 
-    print(f"Data {i}")
-    print("Brier Score:", brier_score)
-    print("RPSS:", rpss_score)
-    print("CRPSS:", crpss_score)
-    print("Compute Pred Loss Time:", pred_loss_end_time - pred_loss_start_time, " sec.")
-
-# 記錄結束時間
-end_time = time.time()
+print("Compute Pred Loss Time:", one_pred_loss_time, " sec.")
+print("Compute Pred Loss (Ignore Compiling) Time:", total_pred_loss_after_compiling_time, " sec.")
 
 # # 計算總共花費的時間
-execution_time = end_time - start_time
-print(f"End2End Execution Time:{execution_time} Sec")
+execution_time = one_var_time + total_pred_loss_time
+execution_ignore_compiling_time = one_var_after_compiling_time + total_pred_loss_after_compiling_time
+print(f"End2End Compute Time :{execution_time} Sec")
+print(f"End2End Compute (Ignore Compiling) Time :{execution_ignore_compiling_time} Sec")
